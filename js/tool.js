8f95912bfc4200ec0adc97544a4285d8eb714543
IvanSidney <ivansidney7@gmail.com>
2022-06-13 13:12:26 +0300

    new 13.06 w1

--- js/tool.js
+++ js/tool.js
@@ -162,12 +162,94 @@
 //     }
 // }
 
-let num = 20;
+// let num = 20;
 
-function showFirstMessege(text) {
-    console.log(text);
-    let num = 10;
-}
+// function showFirstMessege(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
 
-showFirstMessege("Hello World!!!");
-console.log(num);
+// showFirstMessege("Hello World!!!");
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+
+// console.log(anotherNum);
+
+// const logger = function () {
+//     console.log('Hello!!!');
+// };
+
+// logger();
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// };
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return
+//     }
+//     console.log('Done');
+// }
+
+// test();
+
+// function doNothing() {}
+// console.log(doNothing() === undefined);
+
+// const str = "text";
+// // const arr = [1, 2, 3];
+
+// console.log(str.toLocaleUpperCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+// console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
