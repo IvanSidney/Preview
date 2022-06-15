34014feb170b9459223c309dc2118d34a72656a2
IvanSidney <ivansidney7@gmail.com>
2022-06-15 14:29:00 +0300

    new 15.06 w3

--- js/37.js
+++ js/37.js
@@ -12,18 +12,18 @@
 // number = 8;
 // logNumber(); debugger;
 
-function createCounter() {
-    let counter = 0;
+// function createCounter() {
+//     let counter = 0;
 
-    const myFunction = function() {debugger
-        counter = counter + 1;debugger
-        return counter;debugger
-    };
-    return myFunction;
-}
-debugger
-const increment = createCounter();debugger
-const c1 = increment();debugger
-const c2 = increment();debugger
-const c3 = increment();debugger
-console.log(c1, c2, c3);
+//     const myFunction = function() {debugger
+//         counter = counter + 1;debugger
+//         return counter;debugger
+//     };
+//     return myFunction;
+// }
+// debugger
+// const increment = createCounter();debugger
+// const c1 = increment();debugger
+// const c2 = increment();debugger
+// const c3 = increment();debugger
+// console.log(c1, c2, c3);
