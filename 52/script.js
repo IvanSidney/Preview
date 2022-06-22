38525f0cc96bf1f5833ac15dd61f9ed059e96bb7
IvanSidney <ivansidney7@gmail.com>
2022-06-22 10:31:18 +0300

    new 22.06 w1

--- 52/script.js
+++ 52/script.js
@@ -8,16 +8,34 @@
 
 "use strict";
 
+const birthday = Symbol('birthday');
+
 const user = {
     name: 'Alex',
     surname: 'Smith',
-    birthday: '20/04/1993',
+    // birthday: '20/04/1993',
+    [birthday]: '20/04/1993',
     showMyPublicData: function() {
         console.log(`${this.name} ${this.surname}`);
     }
 };
 
-console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+// Object.defineProperty(user, 'birthday', {value: prompt('Date?'), enumerable: true, configurable: true});
 
-Object.defineProperty(user, 'name', {writable: false});
-user.name = 'asfdas';
+// console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
+
+Object.defineProperty(user, 'showMyPublicData', {enumerable: false});
+
+for (let key in user) console.log(key);
+console.log(Object.getOwnPropertyDescriptor(user, birthday));
+
+Object.defineProperties(user, {
+    name: {writable: false},
+    surname: {writable: false}
+});
+
+
+
+// Object.defineProperty(user, 'name', {writable: false});
+// Object.defineProperty(user, 'gender', {value: 'male'});
+// console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
