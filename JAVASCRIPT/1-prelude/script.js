dd7dea465a2220603984fe7e0b245538a02a8af5
IvanSidney <ivansidney7@gmail.com>
2022-07-01 16:00:37 +0300

    new 01.07 w2

--- JAVASCRIPT/1-prelude/script.js
+++ JAVASCRIPT/1-prelude/script.js
@@ -1,3 +1,10 @@
-
-
 "use strict";
+
+let greetingsForUser = "Hello my user";
+
+const greetingsForUser2 = "Hello my dear user";
+
+// alert(greetingsForUser2);
+// alert(greetingsForUser);
+
+console.log(greetingsForUser2);
