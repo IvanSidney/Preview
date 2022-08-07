4cae8be153e0a14e77a116b8f3d09d7f282f19fc
IvanSidney <ivansidney7@gmail.com>
2022-08-07 19:05:03 +0300

    new 07.08 Date

---
+++ 63/script.js
@@ -0,0 +1,15 @@
+const now = new Date();
+
+console.log(now.setHours(18, 40));
+console.log(now);
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
