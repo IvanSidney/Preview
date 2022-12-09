367404396e97fb68adc1045ed8977f59be047b60
IvanSidney <ivansidney7@gmail.com>
2022-12-09 12:14:57 +0200

    new 09.12 Classes

---
+++ 72 Classes/script.js
@@ -0,0 +1,16 @@
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 10);
+
+console.log(square.calcArea());
+console.log(long.calcArea());
