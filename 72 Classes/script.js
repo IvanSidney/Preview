156bc7df62402f86033d6026d5587d64ae6c29af
IvanSidney <ivansidney7@gmail.com>
2022-12-09 12:15:39 +0200

    new 09.12 Classes

--- 72 Classes/script.js
+++ 72 Classes/script.js
@@ -9,8 +9,26 @@ class Rectangle {
     }
 }
 
-const square = new Rectangle(10, 10);
-const long = new Rectangle(20, 10);
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
 
-console.log(square.calcArea());
-console.log(long.calcArea());
+    }
+
+    showMyProps() {
+        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, "Hello World", "red");
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
