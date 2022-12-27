a94e55bff18b3248d2673ce9121d3322d838cc23
IvanSidney <ivansidney7@gmail.com>
2022-12-27 10:09:18 +0200

    new 27/12 arr

---
+++ 81 enumerationOfArrays/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// filter
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+// map
+
+// const answers = ['IvAn', 'AnnA', 'Hello'];
+
+// const result = answers.map(item => item.toLowerCase());
+
+// console.log(result);
+
+// let answers = ['IvAn', 'AnnA', 'Hello'];
+
+// answers = answers.map(item => item.toLowerCase());
+
+// console.log(answers);
+
+
+// every/some
+
+// const some = [4, 'qwq', 'qweqwrfq'];
+
+// console.log(some.some(item => typeof(item) === 'number'));
+// console.log(some.every(item => typeof(item) === 'number'));
+
+// reduce 
+
+// const arr = [4,5,1,3,2,6];
+
+// const res = arr.reduce((sum, current) => sum + current);
+
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+// console.log(res);
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
