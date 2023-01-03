bbaf688abc964e78de4857f49daab59b98844147
IvanSidney <ivansidney7@gmail.com>
2023-01-03 18:15:16 +0200

    new 03/01 jQuery

---
+++ 104jQuery/script.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+    $('.list-item:first').hover(function() {
+        $(this).toggleClass('active');
+    });
+
+    $('.list-item:eq(2)').on('click', function() {
+        $('.image:even').fadeToggle();
+    });
+});
