6e84d32e52eefa93470def167cec569b732915fd
IvanSidney <ivansidney7@gmail.com>
2023-01-04 10:20:44 +0200

    new 04/01 jQuery

--- 104jQuery/script.js
+++ 104jQuery/script.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
     $('.list-item:eq(2)').on('click', function() {
         $('.image:even').fadeToggle();
     });
+
+    $('.list-item:eq(4)').on('click', function() {
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle',
+
+        },2000);
+    });
 });
