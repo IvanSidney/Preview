ed3e88f62fce831727c95b48dc82479869923276
IvanSidney <ivansidney7@gmail.com>
2023-01-09 16:30:29 +0200

    new 08/01 The states

--- Сomponent properties/my-app/src/App.js
+++ Сomponent properties/my-app/src/App.js
@@ -1,14 +1,48 @@
+import {Component} from 'react';
+
 import './App.css';
 
 
 
-function WhoAmI ({name, surname,link}) {
-  return (
-    <div>
-      <h1>My name is {name}, surname - {surname}</h1>
-      <a href={link}>My Profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++',
+      position: ''
+    };
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  commitInputChanges = (e, color) => {
+    console.log(color)
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    const {position, years, text} = this.state;  
+    return (
+      <div>
+        <button onClick={this.nextYear}>{text}</button>
+        <h1>My name is {name}, surname - {surname},
+         age - {years}, position - {position}</h1>
+        <a href={link}>My Profile</a>
+        <form>
+          <span>Enter</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+        </form>
+      </div>
+    )
+  }
 }
 
 function App() {
